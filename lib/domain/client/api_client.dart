1f7cf6614d3847c8490e4bb77579ac5ea7421f35
Yoshiyasu KO <te108128@gmail.com>
2021-05-25 21:30:57 +0900

    modify api_client

--- lib/domain/client/api_client.dart
+++ lib/domain/client/api_client.dart
@@ -1,19 +1,27 @@
-import 'package:dio/dio.dart' as Dio show Response;
+import 'package:dio/dio.dart';
+import 'package:flutter_get_qiita_articles_with_dio/domain/client/api_config.dart';
+import 'package:flutter_get_qiita_articles_with_dio/domain/client/app_dio.dart';
+import 'package:flutter_get_qiita_articles_with_dio/domain/client/request_type.dart';
 import 'package:flutter_get_qiita_articles_with_dio/domain/response/api_error.dart';
-import 'package:flutter_get_qiita_articles_with_dio/domain/response/response.dart';
+import 'package:flutter_get_qiita_articles_with_dio/domain/response/result.dart';
 
 class ApiClient {
   ApiClient._internal();
   static final _instance = ApiClient._internal();
+  factory ApiClient() => _instance;
 
-  Future<Response<T>> sendRequest<T>(
-      {required Future<Dio.Response<Map<String, dynamic>>> request,
+  Future<Result<T>> sendRequest<T>(
+      {required RequestType type,
+      required String path,
       required T Function(Map<String, dynamic>) jsonDecodeCallback}) async {
     try {
-      return await request.then(
-          (response) => Response.success(jsonDecodeCallback(response.data!)));
+      Future<Response<Map<String, dynamic>>> _request = type.when(
+          get: () =>
+              AppDio().get<Map<String, dynamic>>(ApiConfig.endpoint + path));
+      return await _request.then(
+          (response) => Result.success(jsonDecodeCallback(response.data!)));
     } catch (error) {
-      return Response.failure(ApiError.getApiError(error));
+      return Result.failure(ApiError.getApiError(error));
     }
   }
 }
